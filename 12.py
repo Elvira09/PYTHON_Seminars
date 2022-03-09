55d14bb7a0729bbc29b9a8e21d39a7a686cf9029
Elvira09 <zag0909@gmail.com>
2022-03-09 21:08:52 +0300

    12

--- 12.py
+++ 12.py
@@ -3,21 +3,16 @@
 # Для n = 6: {1: 4, 2: 7, 3: 10, 4: 13, 5: 16, 6: 19}
 
 
-# НЕ ДОДЕЛАНО
-
-# n = int(input('Ввведите количество членов словаря:  '))
-# start = int(input('Ввведите стартовую позицию словаря:  '))
-n = 6
-start = 1
-dictionary = {}
-key = range(start, n+1)
-value = range(start * 3 + 1, (n+1) * 3 + 1, 3)
-
-# # for i in key:
-# # 	print(i)
-# # for i in value:
-# # 	print(i)
-
-dictionary = dictionary.fromkeys(key, value)
+n = int(input('Ввведите количество членов словаря:  '))
+start = int(input('Ввведите стартовую позицию словаря:  '))
+# n = 6
+# start = 1
+key = list(range(start, n+1)) 
+value = list(range(start * 3 + 1, (n+1) * 3 + 1, 3)) 
+dictionary = {x: y for x, y in zip(key, value)} # https://docs-python.ru/tutorial/operatsii-slovarjami-dict-python/vyrazhenie-slovar/
 print(dictionary) 
 
+# for i in key:
+# 	print(i)
+# for i in value:
+# 	print(i)
