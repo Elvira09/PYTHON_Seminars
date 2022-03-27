53b711eac59154c6eea7a44a3d9a2e1ae063f66a
Elvira09 <zag0909@gmail.com>
2022-03-27 15:00:29 +0300

    14

--- 14.py
+++ 14.py
@@ -3,13 +3,10 @@
 
 number = float(input('Введите вещественное число:  '))
 # number = 253.8969
-str_number = str(number) #переводим в строку
-str_number = str_number.replace('.', '') #удаляем точку
-# print(str_number)
-list_str = list(str_number) #переводим в список строк 
-# print(list_str)
+str_number = str(number) 
+str_number = str_number.replace('.', '') 
+list_str = list(str_number) 
 list_number = list(map(int, list_str)) #переводим в число каждый элемент списка #https://docs-python.ru/tutorial/vstroennye-funktsii-interpretatora-python/funktsija-map/
-# print(list_number)
 summ = sum(list_number)
 print(summ)
 
