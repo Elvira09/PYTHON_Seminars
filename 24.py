1b94e5a174e3ce9dc73984ededfda0d45a4e7e53
Elvira09 <zag0909@gmail.com>
2022-03-27 14:56:47 +0300

    24

--- 24.py
+++ 24.py
@@ -12,9 +12,8 @@ lst = [40.107, 31.245, 35.17, 5, 10.01]
 lst_temp = []
 for i, x in enumerate(lst) :
     temp = round((x % 1), 3)
-    lst_temp.append(temp)
-# print(lst_temp)
+    if temp != 0: # убираем 0, если в списке присутствует целое число
+        lst_temp.append(temp)
+print(lst_temp)
 diff = max(lst_temp) - min(lst_temp)
 print(diff)
-
-
