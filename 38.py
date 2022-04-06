90ad9d959c8b3ef2b041fa3012416fef3b4a4192
Elvira09 <zag0909@gmail.com>
2022-04-06 21:09:13 +0300

    38

--- 38.py
+++ 38.py
@@ -1,8 +1,10 @@
 # 38. Напишите программу, удаляющую из текста все слова содержащие "абв".
 
+# мой вариант
 text = 'проверка: забвение, зимбабве, операция, 7834, : абв, аБв, итог'
 print(text)
 
+# мой вариант
 list_text = text.split()
 res_text = []
 
@@ -13,4 +15,15 @@ for i in range(len(list_text)):
         res_text.append(list_text[i])
 
 res_text = " ".join(res_text)
+print('# мой вариант')
 print(res_text)
+
+# вариант Дмитрия Лобанова
+text = list(filter(lambda x: 'абв' not in x, text.split()))
+text = " ".join(text)
+print('# вариант Дмитрия Лобанова')
+print(text)
+
+
+
+
